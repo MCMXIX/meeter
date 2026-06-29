e462773a83b97e9a8c132b7676836996ef841e70
MCMXIX <gabriel25han@gmail.com>
2026-06-29 23:09:34 +0800

    adjust docker and vite config

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,20 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        },
+    },
+
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+
+        vue(),
     ],
 });
