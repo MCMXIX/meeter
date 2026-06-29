f1548bc1e6d88786ffbb79e19ed74c18e65f02de
MCMXIX <gabriel25han@gmail.com>
2026-06-30 00:31:12 +0800

    fix ui conflicts issues

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        hmr: {
-            host: 'localhost',
-        },
-    },
-
     plugins: [
         laravel({
-            input: ['resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
 
         vue(),
+
+        tailwindcss(),
     ],
 });
